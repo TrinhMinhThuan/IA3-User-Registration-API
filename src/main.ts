7947fc09a442a05acabbed35f8cb21381093aa39
TrinhMinhThuan <147157853+TrinhMinhThuan@users.noreply.github.com>
2024-10-30 01:15:58 +0700

    Update main.ts
    set anable Cors

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,10 @@
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   await app.listen(process.env.PORT ?? 3000);
-// }
-// bootstrap();
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000', // Địa chỉ frontend React
+    origin: true, // Địa chỉ frontend React
   });
   await app.listen(3003);
 }
