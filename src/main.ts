0259d995155bdea224e073c716508f03415ed445
21120567 <21120567@student.hcmus.edu.vn>
2024-10-30 11:01:02 +0700

    Fix cors

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [`${process.env.LINK_PUBLIC_WEBSITE}`, `${process.env.LINK_LOCALHOST}`], // Địa chỉ frontend React
+  app.use(cors({
+    origin: [`${process.env.LINK_PUBLIC_WEBSITE}`, `${process.env.LINK_LOCALHOST}`],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  });
-  console.log(process.env.LINK_PUBLIC_WEBSITE)
+    allowedHeaders: ['Content-Type', 'Authorization'] 
+  }));  
   await app.listen(3003);
 }
 bootstrap();
